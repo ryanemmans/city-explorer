65b3946f65f1058c4075ada305a58b00aa05b8f4
Ryan Emmans <ryanemmans@gmail.com>
2021-08-23 14:57:11 -0700

    Formatted App.js.

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ export default class App extends Component {
     }
   }
 
-    handleSearchClick = async () => {
-      const searchQuery = 'Seattle';
-      const key = process.env.REACT_APP_CITY_KEY;
+  handleSearchClick = async () => {
+    const searchQuery = 'Seattle';
+    const key = process.env.REACT_APP_CITY_KEY;
 
-      const API_URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${searchQuery}&format=json`;
+    const API_URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${searchQuery}&format=json`;
 
-      const response = await axios.get(API_URL);
+    const response = await axios.get(API_URL);
 
-      this.setState({ location: response.data[0] });
-    }
-
-    render() {
-      return (
-        <>
-          <input />
-          <button onClick={this.handleSearchClick}>Search</button>
-          {this.state.location.place_id &&
-            <h2>The city is: {this.state.location.display_name}</h2>
-          }
-        </>
-      )
-    };
+    this.setState({ location: response.data[0] });
   }
+
+  render() {
+    return (
+      <>
+        <input />
+        <button onClick={this.handleSearchClick}>Search</button>
+        {this.state.location.place_id &&
+          <h2>The city is: {this.state.location.display_name}</h2>
+        }
+      </>
+    )
+  };
+}
