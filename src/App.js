4367df7a0a493bf77de31888d9fe49c313b895c5
Ryan Emmans <ryanemmans@gmail.com>
2021-08-25 19:46:29 -0700

    Rendered weather to page from location search.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import Alert from 'react-bootstrap/Alert';
+import Weather from './weather';
+
 
 export default class App extends React.Component {
   // use constructor and super only if you have state
@@ -22,6 +27,8 @@ export default class App extends React.Component {
     const searchQuery = this.state.searchQuery;
     const key = process.env.REACT_APP_CITY_KEY;
     const API_URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${searchQuery}&format=json&limit=1`;
+
+    // LINES 27 - 34, & 40 disable error
     const weatherRes = await axios.get(`http://localhost:3001/weather`, {
       params: {
         lat: this.state.location.lat,
@@ -30,8 +37,10 @@ export default class App extends React.Component {
       }
     });
     console.log(weatherRes.data);
+    // -----------------------------//
     try {
       const locationRes = await axios.get(API_URL);
+      console.log('Location Data: ', locationRes.data);
       this.setState({
         location: locationRes.data[0],
         alert: '',
@@ -47,33 +56,16 @@ export default class App extends React.Component {
       <Container>
         <Form>
           <Form.Group>
-            <Form.Label
-              style={{ margin: '50px 0px 0px 30px' }}>
+            <Form.Label style={{ margin: '50px 0px 0px 30px' }}>
               <h1>City Explorer</h1>
               <h3>Search For a City:</h3>
             </Form.Label>
-            <Form.Control
-              type="text"
-              placeholder='Enter City Here...'
-              style={{ margin: '10px 0px 20px 30px', width: '30rem' }}
-              onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              value={this.state.searchQuery}
-              onSubmit={e => { e.preventDefault(); }} />
-            <Button
-              // type="reset"
-              variant='info'
-              size='lg'
-              style={{ margin: '0px 0px 20px 30px' }}
-              onClick={this.handleSearchClick}>Explore!</Button>
+            <Form.Control type="text" placeholder='Enter City Here...' style={{ margin: '10px 0px 20px 30px', width: '20rem' }} onChange={(e) => this.setState({ searchQuery: e.target.value })} value={this.state.searchQuery} onSubmit={e => { e.preventDefault(); }} />
+            <Button variant='info' size='lg' style={{ margin: '0px 0px 20px 30px' }} onClick={this.handleSearchClick}>Explore!</Button>
           </Form.Group>
         </Form>
         {this.state.alert ? (
-          <Alert
-            variant={'danger'}
-            style={{
-              margin: '0px 0px 20px 30px',
-              width: '60%'
-            }}>
+          <Alert variant={'danger'} style={{ margin: '0px 0px 20px 30px', width: '60%' }}>
             <Alert.Heading>{this.state.alert}</Alert.Heading>
             <hr />
             <p>
@@ -82,28 +74,27 @@ export default class App extends React.Component {
           </Alert>
         ) : (this.state.location.place_id &&
           <div>
-            <div style={{
-              margin: '0px 0px 20px 30px',
-              border: '1px solid lightGray',
-              borderRadius: '5px',
-              padding: '20px',
-              width: '60%'
-            }}>
-              <h2>Your search: {this.state.location.display_name}</h2>
-              <hr />
-              <h4>Latitude: {this.state.location.lat}</h4>
-              <h4>Longitude: {this.state.location.lon}</h4>
-            </div>
-            <Image
-              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=13`}
-              style={{ boxShadow: '0px 2px 6px gray', margin: '5px 0px 30px 32px' }}
-              rounded />
+            <Card style={{ margin: '0px 0px 20px 30px', border: '1px solid lightGray', borderRadius: '5px', width: '60%'}}>
+              <Card.Header>
+                <h2>Your search: {this.state.location.display_name}</h2>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <h4>Latitude: {this.state.location.lat}</h4>
+                  <h4>Longitude: {this.state.location.lon}</h4>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Row>
+              <Col style={{ display: 'flex', justifyContent: 'space evenly', flexWrap: 'wrap' }}>
+                <Weather forecasts={this.state.forecastData} />
+              </Col>
+            </Row>
+            <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=13`} style={{ boxShadow: '0px 2px 6px gray', margin: '5px 0px 30px 32px' }} alt='map' rounded />
             {/* &markers=icon:|${this.state.location.lat}|${this.state.location.lon}| */}
           </div>
         )}
-      </Container >
+      </Container>
     );
   };
 }
-
-// <Weather this.state.forecastData />
